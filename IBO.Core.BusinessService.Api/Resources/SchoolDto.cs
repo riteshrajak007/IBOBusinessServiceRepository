84433d1dbf301d18b0b0a955b490df5bb2327024
Ritesh <ritesh.rajak@gmail.com>
2020-05-02 11:14:46 +0530

    Added SchoolDto

---
+++ IBO.Core.BusinessService.Api/Resources/SchoolDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBO.Core.BusinessService.Api.Resources
+{
+    public class SchoolDto
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int Country { get; set; }
+
+        public string User { get; set; }
+    }
+}
